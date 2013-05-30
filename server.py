690b89ae31a2250b811a734c742e702fc64ac190
Josh Augustin <augustinspring@gmail.com>
2013-05-30 10:04:35 -0700

    Final version, with a nicely-formatted css directory and file browser

--- server.py
+++ server.py
@@ -10,6 +10,17 @@ server_port = 80
 default_page_path = './'
 error_page_path = './404.html'
 
+stylesheet_path = './main.css'
+stylesheet = None #global
+
+try:
+    sheet = file(stylesheet_path)
+    stylesheet = sheet.read()
+    sheet.close()
+except:
+    print "Failed to load stylesheet from ", stylesheet_path
+
+
 def print_to_socket(socket, msg):
     sent = 0
     msg = msg
@@ -38,11 +49,20 @@ def get_directory_page(dir_path):
     # don't show hidden files
 
     s  = '<html>'
+    if stylesheet != None:
+        s += '<head><style>'
+        s += stylesheet
+        s += '</style></head>'
+
     s += '<body>'
     s += '<h1>{0}</h1>'.format(dir_path)
+    s += '<a href="http://localhost" class="folder">../</a>'
     for f in files_to_display:
-        s += '<a href="{0}">{1}</a>'.format(dir_path+f, f)
-        s += '<br>'
+        file_type = 'file'
+        if os.path.isdir(os.path.join(dir_path, f)):
+            file_type = 'directory'
+            f += '/'
+        s += '<a href="{0}" class="{1}">{2}</a>'.format(dir_path+f, file_type, f)
     s += '</body>'
     s += '</html>'
     return s
@@ -51,8 +71,8 @@ def print_file_to_socket(socket, file_path):
     try:
         (type, encoding) = mimetypes.guess_type(file_path)
         print type, "page requested..."
-        if (file_path == error_page_path):
-            print 'Error 404'
+        if file_path == error_page_path and file_path != '/favicon.ico':
+            print "Error 404: couldn't find", file_path
             #print_to_socket(socket, get_failed_header(type))
         elif type == None: #it's a folder
             print "Folder requested..."
