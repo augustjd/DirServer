45588e288f38670987f56130f11ea01f03d63a87
Josh Augustin <augustinspring@gmail.com>
2013-05-30 08:30:28 -0700

    Now prints a short msg, and is hosted on 80

--- server.py
+++ server.py
@@ -5,21 +5,27 @@ from string import *
 import socket
 from mimetypes import *
 
-server_port = 3000
+server_port = 80
+
+def print_to_socket(socket, msg):
+    sent = 0
+    msg = msg + '\n'
+    total_length = len(msg)
+    while sent < total_length:
+        sent += socket.send(msg[sent:])
+        
 
 def get_server_socket(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
     s.bind(('localhost', port))
     return s
 
-def handle_client(client_msg):
+def handle_client(client):
     # 1: parse http GET header to get requested folder/file
     # 2: obtain file listing of folder, or file, as needed
     # 3: interpolate the HTML file and print it to stdout
-    print 'Content-Type: text/plain'
-    print
-    print
-    print 'Hi there!'
+    print_to_socket(client, 'Hi there!')
+    client.close()
     return 0
 
 
